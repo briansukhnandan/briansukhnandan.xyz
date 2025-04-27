0272a135c9c85f24cbd78737836b702f9166cf1e
Brian Sukhnandan <briansukhnandan@gmail.com>
2025-04-27 01:26:46 -0400

    add a border to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,34 @@ import { SnowEffect, ToggleSnowButton } from "./ui/snow";
 
 export default function Home() {
   const [showSnow, setShowSnow] = useState(true);
+  const borderColor = useGetOscillatingColor();
   return (
     <main>
       {/** Keep SnowEffect as the top-most component */}
       { showSnow ? <SnowEffect /> : null }
-      <OpeningHtmlTag />
-      <ToggleSnowButton
-        toggleFn={() => setShowSnow(!showSnow)}
-      />
-      <Header />
-      <Body />
-      <ClosingHtmlTag/>
+      <Box
+        borderColor={borderColor}
+        borderWidth={2.5}
+      >
+        <OpeningHtmlTag />
+        <ToggleSnowButton
+          toggleFn={() => setShowSnow(!showSnow)}
+        />
+        <Header />
+        <Body />
+        <ClosingHtmlTag/>
+      </Box>
     </main>
   );
 }
 
 const Header = () => {
-  const [colorIdx, setColorIdx] = useState(0);
-  const colors = [
-    "#c53030", // Red 500
-    "#38a169", // Green 500
-  ];
-
-  useEffect(() => {
-    setTimeout(() => 
-      setColorIdx((colorIdx + 1) % colors.length)
-    , 750);
-  }, [colorIdx]);
-
+  const borderColor = useGetOscillatingColor();
   return (
     <Box sx={{
       textAlign: "center",
       paddingTop: "10px",
-      color: colors[colorIdx]
+      color: borderColor
     }}>
       <pre>
         __________       .__                <br/>
@@ -69,3 +64,18 @@ const ClosingHtmlTag = () => (
   </Box>
 );
 
+const useGetOscillatingColor = () => {
+  const [colorIdx, setColorIdx] = useState(0);
+  const colors = [
+    "#c53030", // Red 500
+    "#38a169", // Green 500
+  ];
+
+  useEffect(() => {
+    setTimeout(() => 
+      setColorIdx((colorIdx + 1) % colors.length)
+    , 750);
+  }, [colorIdx]);
+
+  return colors[colorIdx];
+}
