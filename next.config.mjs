7dcee4309b2fc2c3f5fc4207cf9c57443da6ff6b
Brian Sukhnandan <briansukhnandan@gmail.com>
2026-05-03 14:02:41 -0400

    get rid of output: export since it's unnecessary and add a redirect to go to resume from {DOMAIN}/resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  async redirects() {
+    const RESUME_URL =
+      "https://docs.google.com/document/d/1p9_AOGbrJWWhp3-EmGflooyrsfgvVG8EaOxXa0mbgG0"
+
+    return [
+      {
+        source: "/resume",
+        destination: RESUME_URL,
+        // Idk if i want to migrate off of Google Drive at some point
+        // but for now keeping this as non-permanent just in case
+        // the URL changes
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
